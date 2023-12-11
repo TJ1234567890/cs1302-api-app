67139365b23de2003e56b57549704053b822b69c
Tejessrihari Prabakaran <tp03470@uga.edu>
2023-12-11 06:39:31 -0500

    Added api use class

---
+++ src/main/java/cs1302/api/AirportDetails.java
@@ -0,0 +1,13 @@
+package cs1302.api;
+
+/**
+ * Represents a response from the Airport search API. This is called on by Gson to
+ * create an object from the JSON response body. This class is to help in the AppApi file
+ */
+public class AirportDetails {
+    String name;
+    String city;
+    String region;
+    double latitude;
+    double longitude;
+} // AirportDetails
